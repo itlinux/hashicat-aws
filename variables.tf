f2050e1f9511a573183933039116ae27a06443b3
Remo Mattei <remo@mattei.org>
2020-09-15 20:06:16 +0000

    adding cidr var

--- variables.tf
+++ variables.tf
@@ -4,6 +4,10 @@
 # Here is where we store the default values for all the variables used in our
 # Terraform code. If you create a variable with no default, the user will be
 # prompted to enter it (or define it via config file or command line flags.)
+variable "cidr" {
+default = "10.0.0.0/16"
+description = "CIDR"
+}
 
 variable "prefix" {
   description = "This prefix will be included in the name of most resources."
@@ -16,7 +20,7 @@ variable "region" {
 
 variable "address_space" {
   description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
-  default     = ["10.0.0.0/16"]
+  default     = "10.0.0.0/16"
 }
 
 variable "subnet_prefix" {
